f7e0711e7ef37e2ec2e592d2a779289a803b620a
“jimingpengextexit()git config --global user.name “jimingpeng <jimingpeng@wiseweb.com.cn>
2018-03-12 16:46:52 +0800

    ~

---
+++ mysite/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.conf.urls import url, include
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^blog/', include('blog.urls', namespace='blog')),
+    url(r'^account/', include('account.urls', namespace='account')),
+]
---
+++ python36/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Code\Python\KuangJia\Django\mysite\python36\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
