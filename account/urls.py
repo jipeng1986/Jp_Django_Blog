fac1d4112de52a189f926ad206423910955bb54d
“jimingpengextexit()git config --global user.name “jimingpeng <jimingpeng@wiseweb.com.cn>
2018-03-13 16:34:03 +0800

    用户注册

--- account/urls.py
+++ account/urls.py
@@ -4,12 +4,13 @@ from django.conf import settings
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    # 自定义模板
-    # url(r'^login/$', views.user_login, name='user_login')
-
     # 系统内置模板
     # url(r'^login/$', auth_views.login, name='user_login'),
     url(r'^new-login/$', auth_views.login, {"template_name": "account/login.html"}, name='user_login'),
     # url(r'^logout/$', auth_views.logout, name='user_logout'),
-    url(r'^new-login/$', auth_views.logout, {"template_name": "account/logout.html"}, name='user_logout'),
+    url(r'^logout/$', auth_views.logout, {"template_name": "account/logout.html"}, name='user_logout'),
+
+    # 自定义模板
+    # url(r'^login/$', views.user_login, name='user_login'),
+    url(r'^register/$', views.register, name="user_register"),
 ]
